1fc30d90ce71dda82a113c21a0d4959712999816
la2305 <lequangphuoc2305@gmail.com>
2023-11-02 16:59:43 +0700

    i[

---
+++ back_end/not/src/main/java/com/example/not/model/product/Image.java
@@ -0,0 +1,15 @@
+package com.example.not.model.product;
+
+import javax.persistence.*;
+
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long imageId;
+    @Column(columnDefinition = "LONGTEXT")
+    private String imageAddress;
+    @ManyToOne
+    @JoinColumn(name = "product_id",referencedColumnName = "productId")
+    private Product product;
+}
---
+++ back_end/not/src/test/java/com/example/not/NotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.not;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
